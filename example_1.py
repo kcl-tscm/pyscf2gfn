565b913e214fbadac69899199dfd3865baea3888
alejandrosantanabonilla <62341142+alejandrosantanabonilla@users.noreply.github.com>
2020-10-11 10:39:01 +0100

    Update example_1.py

--- example_1.py
+++ example_1.py
@@ -4,15 +4,15 @@ from pyscf2gfn import pyscfxtb
 from pyscf import gto, scf, geomopt
 
 #Geometry optimization using GFn-XTB with minimum parameters
-#pyscfxtb.opt("PATHTOTHEEXECUTABLE/xtb", "ferrocene.xyz")
+pyscfxtb.opt("PATHTOTHEEXECUTABLE/xtb", "ferrocene.xyz")
 
 #Geometry optimization using GFn-XTB with different parameters
-#pyscfxtb.opt("PATHTOTHEEXECUTABLE/xtb", "ferrocene.xyz", threshold="tight")
+pyscfxtb.opt("PATHTOTHEEXECUTABLE/xtb", "ferrocene.xyz", threshold="tight")
 
 #Hessian calculation using GFN-XTB with minimum ammount of information
-#pyscfxtb.hess("PATHTOTHEEXECUTABLE/xtb", "xtbopt.xyz", etemp=0)
+pyscfxtb.hess("PATHTOTHEEXECUTABLE/xtb", "xtbopt.xyz", etemp=0)
 
 #Optimization + Hessian calculation using GFN-XTB
-#pyscfxtb.opthess("PATHTOTHEEXECUTABLE/xtb", "xtbopt.xyz",version=1)
+pyscfxtb.opthess("PATHTOTHEEXECUTABLE/xtb", "xtbopt.xyz",version=1)
 
 
